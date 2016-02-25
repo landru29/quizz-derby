bdd79e62913f16bf5231bcca485d805148c8ccd6
Cyrille M <cmeichel@free.fr>
2016-02-25 09:01:38 +0100

    button style & answer check

--- src/app/components/mcq/mcq.component.js
+++ src/app/components/mcq/mcq.component.js
@@ -5,6 +5,25 @@ angular.module('App').component('mcq', {
     controllerAs: 'Mcq',
     templateUrl: 'app/components/mcq/mcq.html',
     controller: function() {
+        'use strict';
 
+        var self = this;
+
+        this.computeAnswers = function() {
+                self.model.choices.forEach(function(choice) {
+                    choice.checked = false;
+                });
+                if (self.answer) {
+                    if (self.answer.single) {
+                        self.model.choices[self.answer.single].checked = true;
+                    }
+                    if (self.answer.multi) {
+                        Object.keys(self.answer.multi).forEach(function(index) {
+                            self.model.choices[index].checked = self.answer.multi[index];
+                        });
+                    }
+                }
+                self.model.done = true;
+        };
     }
 });
