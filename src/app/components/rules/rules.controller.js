ed7a6ecaeb59f4f2b24d7d490efe328977759e57
Cyrille M <cmeichel@free.fr>
2016-02-27 12:53:22 +0100

    Rules controller

--- src/app/components/rules/rules.controller.js
+++ src/app/components/rules/rules.controller.js
@@ -1,44 +1,58 @@
 angular.module('App').controller('RulesCtrl',
-	function($stateParams, $resource, $sce) {
+	function($stateParams, $resource, $sce, $timeout, $location, $anchorScroll, $q) {
 		'use strict';
 
 		var self = this;
 
         var res = $resource(
-            "/assets/rules/:year/:page",
+            '/assets/rules/:year/:page',
             {},
             {
                 get:{
+					url: '/assets/rules/:year/:page',
                     isArray: false,
                     interceptor: {
                         response: function(resp) {
                             return $sce.trustAsHtml(resp.data);
                         }
                     }
-                }
+                },
+				index: {
+					url: '/assets/rules/:year/content.json',
+                    isArray: false
+				}
             }
         );
 
-        function loadPage(year, subtitle) {
-            return res.get({
-                year: year,
-                page: subtitle + ".html"
-            }).$promise.then(function(dataHtml) {
-                self.html = dataHtml;
-            });
+        function loadPage(year, subtitle, section, sub) {
+			return $q.all([
+				res.get({
+	                year: year,
+	                page: subtitle + '.html'
+	            }).$promise,
+				res.index({
+	                year: year
+	            }).$promise
+			]).then(function(data) {
+				self.html = data[0];
+				self.content = data[1];
+				self.h1 = self.content[sub[0]];
+				if (self.h1) {
+					self.h2 = self.h1.sub[sub[1]];
+				}
+				$timeout(function() {
+					$location.hash(section);
+            		$anchorScroll();
+				});
+			});
         }
 
 		function init() {
             self.year = $stateParams.year.replace(/[^\d]/g, '');
-            var matcher = $stateParams.subtitle.match(/^(\d*)[^\d](\d*)/);
-            var subtitle;
-            if (matcher) {
-                var t1 = '00' + matcher[1];
-                var t2 = '00' + matcher[2];
-                subtitle = t1.substr(-2) + '_' + t2.substr(-2);
-                self.subtitle = subtitle.replace(/_/, '.');
-            }
-            return loadPage(self.year, subtitle);
+			var subtitles = $stateParams.subtitle.split(/[^\d]/);
+			var t1 = ('00' + subtitles[0]).substr(-2);
+			var t2 = ('00' + subtitles[1]).substr(-2);
+            return loadPage(self.year, t1 + '_' + t2, 'section_' + subtitles.join('_'), subtitles);
 		}
 
 		init();
